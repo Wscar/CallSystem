ccaaec4a72456c575680e170c2fa149ae8f2fed9
YeMobai <546307885@qq.com>
2019-10-24 00:01:13 +0800

    添加identyserver4认证服务

---
+++ src/StupidProject/AuthenticationCentre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthenticationCentre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
